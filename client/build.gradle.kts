4d6977246b3584b22ed888637cea2d5d81baf684
version-bump <version-bump@flossware.org>
2026-07-18 15:13:31 +0000

    [ci skip] rev version to 0.15

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.flossware.hotspot.client"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 14
-        versionName = "0.14"
+        versionCode = 15
+        versionName = "0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
